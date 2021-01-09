a539cfe1d0841cdf122571b79e75b54a942f7b57
Your Name <you@example.com>
2021-01-09 17:32:17 +0530

    firstcommit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    hello world
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <api/>
+  
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import api from './components/api.vue'
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    api
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/api.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="content">
+           <button @click="created()">get request</button>
+         
+    <button @click="put()">put request</button>
+    <button @click="post()">post request</button>
+    <button @click="patch()">patch request</button>
+    <button @click="delet()">delete request</button><hr>
+    <label>Enter User To ADD</label>
+    <input type="text" v-model="newUser" @keyup="addUser">
+       <li>{{datas}}</li>
+   
+               
+  
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: 'api',
+  data(){
+      return{
+          datas:[],
+          newUser:''
+      }
+  },
+mounted(){
+
+},
+methods:{
+  created() {
+    axios.get("https://reqres.in/api/users?page=2").then((result) => {
+      console.log(result.data);
+      this.datas = result.data
+    })
+  },
+  put() {
+    axios.put("https://reqres.in/api/users/2",{
+        "name":"rama",
+        "job":"leader"
+    }).then((result) => {
+      console.log(result.data);
+      this.datas = result.data
+    })
+  },
+   post() {
+    axios.post("https://reqres.in/api/users",{
+        "name":"rama",
+        "job":"leader"
+    }).then((result) => {
+      console.log(result.data);
+      this.datas = result.data
+    })
+  },
+   patch() {
+    axios.patch("https://reqres.in/api/users/2",{
+        "name":"rama",
+        "job":"notleader"
+    }).then((result) => {
+      console.log(result.data);
+      this.datas = result.data
+    })
+   },
+    delet() {
+    axios.delete("https://reqres.in/api/users/2").then((result) => {
+      console.log(result.data);
+      this.datas = result.data
+    })
+  },
+  addUser(){
+      axios.post("https://reqres.in/api/users",{
+          name:this.newUser
+      }).then((result)=>{
+          const data =result.data;
+          this.datas.push(data);
+          this.newUser=""
+      })
+  }
+}
+};
+
+</script>
+
+<style scoped lang="scss">
+</style>
